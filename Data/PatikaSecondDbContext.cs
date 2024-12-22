34ade281db8d38fe937dc6045fcf63b732a7f858
OrkunDemir5 <orkun.d1999@gmail.com>
2024-12-22 13:19:30 +0300

    Proje dosyası ekle.

---
+++ Data/PatikaSecondDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstRelationExample.Data
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=PatikaCodeFirstDb2");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // User - Post Relationship
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.User) 
+                .WithMany(u => u.Posts) 
+                .HasForeignKey(p => p.UserId); 
+            //modelBuilder.Entity<Post>().HasData(
+            //    new Post() { Title = "Ilk Post", Content = "Hello World!" },
+            //    new Post() { Title = "Ikinci Post", Content = "Hello Moon!" }
+            //    );
+            //modelBuilder.Entity<User>().HasData(
+            //    new User() { Username = "Orkun Demir", Email = "orkun.d1999@gmail.com" },
+            //    new User() { Username = "Ahmet Mehmet", Email = "ahmetmehmet9@gmail.com" }
+            //    );
+        }
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstRelationExample.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        // Navigation Property
+        public ICollection<Post> Posts { get; set; }
+    }
+}
